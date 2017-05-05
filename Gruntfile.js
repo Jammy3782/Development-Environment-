27f1242f3a60e022f69caef6efd923af91de71d5
Jamila Youngblood <jamilayoungblood@Jamilas-MacBook-Pro.local>
2017-05-04 18:21:55 -0700

    added grunt build control

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function (grunt) {
 
   // Automatically load required grunt tasks
   require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin'
+    useminPrepare: 'grunt-usemin',
+    buildcontrol: "grunt-build-control"
   });
 
   // Configurable paths
@@ -27,8 +28,29 @@ module.exports = function (grunt) {
   // Define the configuration for all the tasks
   grunt.initConfig({
 
+    // # Globbing
+    // for performance reasons we're only matching one level down:
+    // 'test/spec/{,*/}*.js'
+    // If you want to recursively match all subfolders, use:
+    // 'test/spec/**/*.js'
+
     // Project settings
     config: config,
+
+    buildcontrol: {
+     options: {
+       dir: 'dist',
+       commit: true,
+       push: true,
+       message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+     },
+     pages: {
+       options: {
+         remote: 'git@github.com:Jammy3782/Development-Environment-.git',
+         branch: 'gh-pages'
+       }
+     }
+   },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
